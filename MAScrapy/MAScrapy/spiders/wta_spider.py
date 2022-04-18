909d2b9101ee3091136168a3a26ae8c4196d6500
mchielouis <michelle.flanner@gmail.com>
2022-04-18 03:43:30 -0700

    Updated user agent in settings.py again. Implemented wta_spider to now follow next page link and recursively scrape hike item header info, also catching IndexError thrown if no region_2.

--- MAScrapy/MAScrapy/spiders/wta_spider.py
+++ MAScrapy/MAScrapy/spiders/wta_spider.py
@@ -2,12 +2,22 @@ import scrapy
 
 class wta_spider(scrapy.Spider):
     name='wta_spider'
-    start_urls=['https://www.wta.org/go-outside/hikes']
+    start_urls=['https://www.wta.org/go-outside/hikes'] #start_urls is a shortcut for start_requests()
 
     def parse(self,response):
-        for hike_header in response.css('div.item-header'):
-            yield {
-                'HIKE_NAME': hike_header.xpath('.//a/span/text()').get(),
-                'REGION_1': hike_header.xpath('.//h3/text()').get().rsplit('--')[0].strip(),
-                'REGION_2': hike_header.xpath('.//h3/text()').get().rsplit('--')[1].strip()
-            }
+        for hike_header in response.css('div.item-header'): #looping over hike item blocks
+            try:
+                yield {
+                    'HIKE_NAME': hike_header.xpath('.//a/span/text()').get(),
+                    'REGION_1': hike_header.xpath('.//h3/text()').get().rsplit('--')[0].strip(),
+                    'REGION_2': hike_header.xpath('.//h3/text()').get().rsplit('--')[1].strip()
+                }
+            except IndexError:
+                yield {
+                    'HIKE_NAME': hike_header.xpath('.//a/span/text()').get(),
+                    'REGION_1': hike_header.xpath('.//h3/text()').get().rsplit('--')[0].strip(),
+                    'REGION_2': ''
+                }
+        next_page = response.css('li.next > a').xpath('./@href').get() #getting next page link
+        if next_page:
+            yield scrapy.Request(next_page, callback=self.parse) #recursively parsing each page
