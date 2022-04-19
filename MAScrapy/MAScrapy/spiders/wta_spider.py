ce02246c709abe1271e121972d99d7713c79779c
mchielouis <michelle.flanner@gmail.com>
2022-04-18 20:43:17 -0700

    Implemented wta_spider to now scrape all the data I want for each hike. Next step is to implement a scrapy item to better handle missing fields, unexpected parse errors, etc. Current issue is encountering missing LENGTH field.

--- MAScrapy/MAScrapy/spiders/wta_spider.py
+++ MAScrapy/MAScrapy/spiders/wta_spider.py
@@ -1,16 +1,30 @@
 import scrapy
+import re
 
 class wta_spider(scrapy.Spider):
     name='wta_spider'
     start_urls=['https://www.wta.org/go-outside/hikes'] #start_urls is a shortcut for start_requests()
 
     def parse(self,response):
-        for hike_header in response.css('div.item-header'): #looping over hike item blocks
+        i = 0
+        for hike_item in response.css('div.search-result-item'): #looping over hike item blocks
+            hike_header = hike_item.css(' div.item-header')
+            hike_stats = hike_item.css(' div.hike-stats.alpha > div')
+            i += 1
             try:
                 yield {
+                    'ID': i,
                     'HIKE_NAME': hike_header.xpath('.//a/span/text()').get(),
                     'REGION_1': hike_header.xpath('.//h3/text()').get().rsplit('--')[0].strip(),
-                    'REGION_2': hike_header.xpath('.//h3/text()').get().rsplit('--')[1].strip()
+                    'REGION_2': hike_header.xpath('.//h3/text()').get().rsplit('--')[1].strip(),
+                    'LENGTH': float(hike_stats.css('div.hike-length > span::text').get().rsplit(',')[0].split(' ')[0]),
+                    'ROUNDTRIP': True if hike_stats.css('div.hike-length > span::text').get().rsplit(',')[1].strip() == 'roundtrip' else False,
+                    'GAIN': int(hike_stats.css('div.hike-gain > span::text').get().rsplit(' ')[0]),
+                    'HIGHPOINT': int(hike_stats.css('div.hike-highpoint > span::text').get().rsplit(' ')[0]),
+                    'RATING': float(hike_stats.css('div.current-rating::text').get()),
+                    'VOTES': int(re.findall(r'\d+',hike_stats.css('span.rating-count::text').get())[0]),
+                    'FEATURES': hike_stats.css('div.trip-features img::attr(title)').getall(),
+                    'DESC': re.sub('(\u2018|\u2019)',"'",hike_item.css('div.listing-summary.omega.show-excerpt::text').get().strip())
                 }
             except IndexError:
                 yield {
